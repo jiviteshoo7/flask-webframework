638fa1d89b0a1c3b26b27426d7d489beb9295368
jivitesh varshney <jiviteshkumar.jk007@gmail.com>
2023-04-26 15:52:11 +0530

    web update

---
+++ main.py
@@ -0,0 +1,51 @@
+from flask import Flask,redirect,url_for,render_template,request
+app = Flask(__name__)
+
+@app.route('/')
+def func():
+    return render_template('index.html')
+
+
+@app.route('/success/<int:score>')
+def success(score):
+    res = ""
+    if score >= 50:
+        res = "PASS"
+    else:
+        res = "FAIL"
+    exp = {'score':score,'res':res}
+    return render_template('result.html',result=exp)
+
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return 'The person has failed and the score is'+ str(score)
+
+
+## results checker
+@app.route('/results/<int:marks>')
+def results(marks):
+    status = ""
+    if marks < 50:
+        status = 'fail'
+    else:
+        status = 'success'
+    return redirect(url_for(status,score=marks))
+
+
+##result checker html page
+@app.route('/submit',methods=['POST','GET'])
+def submit():
+    total_score = 0
+    if request.method=='POST':
+        science = float(request.form['science'])
+        maths = float(request.form['maths'])
+        c = float(request.form['c'])
+        data_science = float(request.form['datascience'])
+        total_score = (science+maths+c+data_science)/4
+    return redirect(url_for('success',score=total_score))
+    
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
